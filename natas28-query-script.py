4e131ed9d66eb02db6de45fe4d10277c10fed34a
otherness <ojofuffu@gmail.com>
2017-11-05 13:39:10 +0000

    Python 3 migration
    Change-Id: I36e7832f532d1ee4dfcb823847e6056d3af1aecd

--- natas28-query-script.py
+++ natas28-query-script.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/local/bin/python3
 
 
 import requests,sys,base64,urllib,binascii
@@ -37,13 +37,13 @@ def xor_hex_str(str1,str2):
 def encode_payload(pld):
     payload_ascii = binascii.unhexlify(pld)
     payload_base64 = base64.b64encode(payload_ascii)
-    return urllib.quote(payload_base64, safe ='')
+    return urllib.parse.quote(payload_base64, safe ='')
 
 def decode_answer(ans):
-    ans_base64 = urllib.unquote(ans)
+    ans_base64 = urllib.parse.unquote(ans)
     ans_ascii = base64.b64decode(ans_base64)
-    #print ans_base64
-    return binascii.hexlify(ans_ascii)
+    #print(ans_base64)
+    return str(binascii.hexlify(ans_ascii), 'ascii')
 
 
 def assemble_payload(index,modified_prev_block):
@@ -59,18 +59,18 @@ def assemble_payload(index,modified_prev_block):
 #block is string in hex format XX without leading 0x
 def divide_block_to_bytes(block):
     n = 2 #two chars for each byte
-    print "Dividing block " + block
+    print("Dividing block " + block)
     block_byte_list = [block[i:i+n] for i in range (0, len(block), n)]
     return block_byte_list
 
 
 def apply_padding_xor(byte_list, pad_size):
     xored_byte_list = byte_list[:-pad_size]
-    print "DEBUG: Applying padding to block:", byte_list
-    print "DEBUG: Padding size:", pad_size
+    print("DEBUG: Applying padding to block:", byte_list)
+    print("DEBUG: Padding size:", pad_size)
     pad_size_str = str(format(pad_size, '02x')) #convert padding byte to string
-    print "DEBUG: Padding in hex: " + pad_size_str
-    print "DEBUG: Applying padding to last", pad_size, "bytes only:", byte_list[-pad_size:]
+    print("DEBUG: Padding in hex: " + pad_size_str)
+    print("DEBUG: Applying padding to last", pad_size, "bytes only:", byte_list[-pad_size:])
     for byte in byte_list[-pad_size:]:
         xored_byte_list.append(xor_hex_str(byte, pad_size_str))
     return xored_byte_list
@@ -79,10 +79,10 @@ def apply_padding_xor(byte_list, pad_size):
 
 def xor_guess_and_guessed_bytes(g, ind, pad_prev_block, guessed_block):
     guess_xored_list = pad_prev_block[:]
-    print "DEBUG: XOR with guess", g, "on position", ind, "to padded block", pad_prev_block
+    print("DEBUG: XOR with guess", g, "on position", ind, "to padded block", pad_prev_block)
     guess_xored_list[ind] = xor_hex_str(g, pad_prev_block[ind])
-    print "DEBUG: Result after guess applied:", guess_xored_list
-    print "DEBUG: XOR with previously guessed ", guessed_block
+    print("DEBUG: Result after guess applied:", guess_xored_list)
+    print("DEBUG: XOR with previously guessed ", guessed_block)
     assert len(guessed_block) == len(guess_xored_list)
     payload_block_list = []
     for i in range(0,len(guess_xored_list)):
@@ -93,12 +93,13 @@ def xor_guess_and_guessed_bytes(g, ind, pad_prev_block, guessed_block):
 
 
 def print_hex(string):
+    print(string)
     base = 32
-    for i in range (len(string)/32):
+    for i in range (len(string)//32):
         if i%2 == 0:
-            print bcolors.OKBLUE + string[base*i:base*(i+1)] + bcolors.ENDC,
+            print(bcolors.OKBLUE + string[base*i:base*(i+1)] + bcolors.ENDC,)
         else:
-            print bcolors.OKGREEN + string[base*i:base*(i+1)] + bcolors.ENDC,
+            print(bcolors.OKGREEN + string[base*i:base*(i+1)] + bcolors.ENDC,)
     print
 
 #input_list = ["A", "AA", "AAA", "AAAA", "AAAAA", "AAAAAA", "AAAAAAA",\
@@ -109,12 +110,13 @@ input_list = ["", "&", "%26", "a", "b", "'", "%27", "BBBBBBBBBBAAAAAAAAAAAAA", "
 
 
 for query in input_list:
-    print query, len(query)
+    print(query, len(query))
     r = requests.post(url_post, auth = cred, proxies = prx, headers = hdrs, data = "query="+query, allow_redirects=False)
-    loc_header = r.headers['Location']
+    loc_header = r.headers['Location'] # check position of equals sign in the Location header (after query=)
     equal_sign_pos = loc_header.find('=') + 1
-    print urllib.unquote(loc_header[equal_sign_pos:])
+    print(urllib.parse.unquote(loc_header[equal_sign_pos:])) # print URL-decoded query parameter
     ans_hex = decode_answer(loc_header[equal_sign_pos:])
+    print(ans_hex)
     #print ans_hex
     print_hex(ans_hex)
     #print divide_block_to_bytes(ans_hex)
@@ -123,10 +125,10 @@ for query in input_list:
 hex_get_query = "b69b50e5736aad13c5d63e684eefbb29"
 hex_get_query += "d8ae51d7da71b2b083d919a0d7b88b98" #append full block of PKCS#7 padding
 r = requests.get(url_get + encode_payload(hex_get_query), auth = cred, proxies = prx)
-print r.content
+print(r.content)
 hex_get_query = "1be82511a7ba5bfd578c0eef466db59cdc84728fdcf89d93751d10a7c75c8cf2ab880a8f136fbeb98967891324a1b075bdfa1054ec68515cf96f2a5544591947904f4b2abf2c2d7686aa72a53151c970"
 r = requests.get(url_get + encode_payload(hex_get_query), auth = cred, proxies = prx)
-print r.content
+print(r.content)
 
 
 # post_query_hex = "f633e6b05f866226b863817112b1c92b"
